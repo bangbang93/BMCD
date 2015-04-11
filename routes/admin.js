58c3b103670672a8b2cd970fcccf76de8083c665
bangbang93 <bangbang93@163.com>
2015-04-11 17:19:47 +0800

    reformat

--- routes/admin.js
+++ routes/admin.js
@@ -9,15 +9,15 @@ var Server = require('../modules/server');
 var Config = require('../modules/config');
 var User = require('../modules/user');
 
-router.use(function (req, res, next){
-    if (!req.isLogin || !req.isAdmin){
-        res.send(403);
-    } else {
-        next();
-    }
+router.use(function (req, res, next) {
+  if (!req.isLogin || !req.isAdmin) {
+    res.send(403);
+  } else {
+    next();
+  }
 });
 
-router.post('/server/create', function (req, res){
+router.post('/server/create', function (req, res) {
   var server = [];
   var name = req.param('name');
   var host = req.param('host');
@@ -25,62 +25,62 @@ router.post('/server/create', function (req, res){
   var path = req.param('path');
   var file = req.param('file');
   var args = req.param('args') || [];
-  if (!name || !host || !port || !path || !file){
+  if (!name || !host || !port || !path || !file) {
     return res.send(400);
   }
-  Server.createServer(name, host, port, path, file, function (err, success){
-    if (err){
-      if (err.errno == 34){
+  Server.createServer(name, host, port, path, file, function (err, success) {
+    if (err) {
+      if (err.errno == 34) {
         res.send(404);
       } else {
         res.json(500, err);
       }
     } else {
-        res.send(204);
+      res.send(204);
     }
   })
 });
 
-router.post('/configure', function (req, res){
-    var java = req.param('java');
-    Config.set('java', java);
-    res.send(204);
+router.post('/configure', function (req, res) {
+  var java = req.param('java');
+  Config.set('java', java);
+  res.send(204);
 });
 
-router.get('/configure', function (req, res){
-    Config.getAll(function (err, config){
-        res.json(config);
-    })
+router.get('/configure', function (req, res) {
+  Config.getAll(function (err, config) {
+    res.json(config);
+  })
 });
 
-router.get('/status', function (req, res){
-    res.json({
-        uptime: process.uptime(),
-        uid: process.getuid(),
-        gid: process.getgid(),
-        arch: process.arch,
-        platform: process.platform,
-        memoryUsage: process.memoryUsage()
-    })
+router.get('/status', function (req, res) {
+  res.json({
+    uptime: process.uptime(),
+    uid: process.getuid(),
+    gid: process.getgid(),
+    arch: process.arch,
+    platform: process.platform,
+    memoryUsage: process.memoryUsage()
+  })
 });
 
-router.get('/userList', function (req, res){
-    User.listUser(function (users){
-        res.json(users);
-    })
+router.get('/userList', function (req, res) {
+  User.listUser(function (users) {
+    res.json(users);
+  })
 });
 
-router.post('/changePassword', function (req, res){
-    var username = req.param('username');
-    var password = req.param('password');
+router.post('/changePassword', function (req, res) {
+  var username = req.param('username');
+  var password = req.param('password');
 
-    User.changePassword(username, password, function (err){
-        if (err){
-            res.json(500, err);
-        } else {
-            res.send(204);
-        }
-    })
+  User.changePassword(username, password, function (err) {
+    if (err) {
+      res.json(500, err);
+    } else {
+      res.send(204);
+    }
+  })
 });
 
 module.exports = router;
