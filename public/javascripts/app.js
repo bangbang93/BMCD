7d77300e18e85bd8c4bba58bcc10c8f57f774531
bangbang93 <bangbang93@163.com>
2015-06-15 16:17:52 +0800

    使用angularjs包装socket.io

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -20,8 +20,32 @@ angular
       }).when('/server/:sid', {
         templateUrl: '/views/server/info.html',
         controller: 'ServerInfoCtrl'
+      }).when('/server/:sid/console', {
+        templateUrl: '/views/server/console.html',
+        controller: 'ServerConsoleCtrl'
       })
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .factory('io', function ($rootScope) {
+    var socket = io.connect();
+    return {
+      on: function (event, cb) {
+        socket.on(event, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            cb.apply(socket, args);
+          })
+        })
+      },
+      emit: function (event, data, cb) {
+        socket.emit(event, data, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            cb&&cb.apply(socket, args);
+          })
+        })
+      }
+    }
   });
--- public/javascripts/controllers/server.js
+++ public/javascripts/controllers/server.js
@@ -1,7 +1,8 @@
 /**
  * Created by bangbang93 on 15-4-11.
  */
-angular.module('bmcdApp').controller('ServerInfoCtrl', function ($scope, $http, $route){
+angular.module('bmcdApp')
+  .controller('ServerInfoCtrl', function ($scope, $http, $route){
   $scope.$on('$routeChangeSuccess', function (ev, current, prev){
     $http.get('/server/info/' + current.params.sid).success(function (data){
       $scope.server = data;
@@ -20,9 +21,57 @@ angular.module('bmcdApp').controller('ServerInfoCtrl', function ($scope, $http, 
       })
     })
   });
+  $scope.goto = function (type) {
+    switch (type){
+      case 'console':
+        console.log($route);
+        window.location = '#/server/' + $scope.server._id + '/console';
+    }
+  };
   $scope.start = function (){
     $http.get('/server/start/' + $route.current.params.sid).success(function (data){
 
     })
   }
-});
+})
+  .controller('ServerConsoleCtrl', function ($scope, $http, $route) {
+    var server = window.location.hash.substr(1);
+    var socket = io('/server');
+    socket.on('connect', function (){
+      socket.emit('init', {
+        server: server
+      })
+    });
+    $(document).ready(function (){
+      var $command = $('#command');
+      var $console = $('#console');
+      var $submit = $('#submit');
+      var textareaConsole = document.getElementById('console');
+      $submit.on('click', function (){
+        var command = $command.val();
+        if (!command){
+          return;
+        }
+        socket.emit('command', command);
+        $console.append('\n' + new Date().toLocaleString() + '  执行命令：' + command + '\n');
+        $command.val('');
+      });
+      $command.on('keydown', function (key){
+        if (key.which == 13){
+          $submit.click();
+        }
+      });
+      socket.on('console', function (data){
+        $console.append(data);
+        textareaConsole.scrollTop = textareaConsole.scrollHeight;
+      });
+      socket.on('history', function (data){
+        var str = '';
+        data.forEach(function (e){
+          str += data;
+        });
+        $console.append(str);
+        textareaConsole.scrollTop = textareaConsole.scrollHeight;
+      });
+    })
+  })
